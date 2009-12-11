ddcd7c1cb4f25d1171f483d9e25b200b0ab6cdc1
Cyrille Pontvieux <jrdATenialisDOTnet>
2009-12-11 22:19:54 +0000

    FIX: correct versionning package for the trunk of lilosetup and salix-live-installer FIX: some fixes for salix-live.

--- maketxz.sh
+++ maketxz.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 mkdir -p pkg
 export DESTDIR=$PWD/pkg
 ./install.sh
-VER=$(grep 'Version=' src/salix-live-installer | cut -d\' -f2)
+VER=$(grep 'Version=' src/salix-live-installer | sed "s/.*Version='\(.*\)'.*- \([0-9]*\).*/\1.\2/")
 cd pkg
 makepkg -l y -c n ../salix-live-installer-$VER-noarch-1plb.txz
 cd ..
