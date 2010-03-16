fae7cd17b0d2b11c5202207893fc2ea04de337ea
Cyrille Pontvieux <jrdATenialisDOTnet>
2010-03-16 11:16:21 +0000

    Salix Live RC3

--- maketxz.sh
+++ maketxz.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 mkdir -p pkg
 export DESTDIR=$PWD/pkg
 ./install.sh
-VER=$(grep 'version =' src/salix-live-installer.py | head -n 1 | sed "s/.*'\(.*\)' - \([0-9]*\).*/\1.\2/")
+VER=$(grep 'version =' src/salix-live-installer.py | head -n 1 | sed "s/.*'\(.*\)' - .*/\1/")
 cd pkg
 cat <<EOF > install/slack-desc
 salix-live-installer: Salix Live Installer - Install Salix Standard from LiveCD.
