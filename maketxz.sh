0181194fa1a7a65c41f2c501e4a7743bcc63b4b6
Cyrille Pontvieux <jrdATenialisDOTnet>
2011-09-19 12:15:01 +0000

    RLZ variable and change the salix-live-installer version

--- maketxz.sh
+++ maketxz.sh
@@ -6,6 +6,7 @@ mkdir -p pkg
 export DESTDIR=$PWD/pkg
 ./install.sh
 VER=$(grep 'version =' src/salix-live-installer.py | head -n 1 | sed "s/.*'\(.*\)'/\1/")
+RLZ=1plb
 cd pkg
 cat <<EOF > install/slack-desc
 salix-live-installer: Salix Live Installer - Install Salix Standard from LiveCD.
@@ -20,8 +21,8 @@ salix-live-installer:
 salix-live-installer:
 salix-live-installer:
 EOF
-makepkg -l y -c n ../salix-live-installer-$VER-noarch-1plb.txz
+makepkg -l y -c n ../salix-live-installer-$VER-noarch-$RLZ.txz
 cd ..
-md5sum salix-live-installer-$VER-noarch-1plb.txz > salix-live-installer-$VER-noarch-1plb.md5
-echo -e "gparted,python,salixtools" > salix-live-installer-$VER-noarch-1plb.dep
+md5sum salix-live-installer-$VER-noarch-$RLZ.txz > salix-live-installer-$VER-noarch-$RLZ.md5
+echo -e "gparted,python,salixtools" > salix-live-installer-$VER-noarch-$RLZ.dep
 rm -rf pkg
