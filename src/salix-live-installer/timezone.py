bb56d22edba3c397eceb259ef4d4884075ad955b
Cyrille Pontvieux <cyrille.pontvieux@april-waf.com>
2013-02-06 11:52:48 +0100

    Timezone will be the last module to implement.

--- src/salix-live-installer/timezone.py
+++ src/salix-live-installer/timezone.py
@@ -2,5 +2,21 @@
 # -*- coding: utf-8 -*-
 # vim: set et ai sta sw=2 ts=2 tw=0:
 """
-Functions to handle timezones
+Functions to handle timezones:
+  - listTimeZones
+  - listTZContinents
+  - listTZCountries
+  - getDefaultTimeZone
+  - setDefaultTimeZone
 """
+import os
+import glob
+import re
+import fileinput
+import sys
+from execute import *
+
+# Unit test
+if __name__ == '__main__':
+  from assertPlus import *
+  checkRoot()
